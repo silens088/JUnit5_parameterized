211bcace5623dfefb2a961c478e6983235acc4bb
Vitalii_Velich <silens088@gmail.com>
2021-12-07 23:51:04 +0300

    add В идее Help Edit custov VM добавить две строчки -Dconsole.encoding=UTF-8 -Dfile.encoding=UTF-8

--- src/test/java/vitalii/Junit5/tests/PolkaTest.java
+++ src/test/java/vitalii/Junit5/tests/PolkaTest.java
@@ -7,13 +7,15 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 import static vitalii.Junit5.tests.DataTests.*;
 
-    @Owner("velichkovv")
-    @Feature("параметризованные тесты")
+@Owner("velichkovv")
+@Feature("параметризованные тесты")
 
 public class PolkaTest {
 
@@ -37,7 +39,22 @@ public class PolkaTest {
 
         $("._1Vhx8").shouldHave(text(bookAuthor)).shouldBe(visible);
         $("._3j3bx").shouldHave(text(bookName)).shouldBe(visible);
+    }
 
+    @Tag("param")
+    //@DisplayName("Search book")
+    @ValueSource(strings = {"Анна Каренина", "Война и мир", "Вишнёвый сад"} )
+    @ParameterizedTest(name = "Search {0} book")
+    public void searchBookTest(String selectBook) {
+        open("https://polka.academy/");
+
+        $(".wCizZ svg").click();
+        $(".AKPsr").setValue(selectBook).pressEnter();
+        $("#searchInput input").shouldHave(value(selectBook)).shouldBe(visible);
+        $(".mkXom").click();
+
+        //$("._1Vhx8").shouldHave(text(selectAuthor)).shouldBe(visible);
+        $(".uO74Z h1").shouldHave(text(selectBook)).shouldBe(visible);
 
     }
 }
